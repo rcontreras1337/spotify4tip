f3ad3b4c96da9ac6cb4760a9897c9f1d6dde3b57
Ruben Contreras <ruben4tip@hotmail.com>
2023-03-30 03:44:53 -0300

    ejemplos rutas

--- spotify/src/app/app-routing.module.ts
+++ spotify/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', // ESTO ES LA RUTA ROOT osea localhost:4200
+    component: ExampleComponent
+  },
+  {
+    path: 'hello', // ESTO ES LA RUTA ROOT osea localhost:4200
+    component: ExampleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
