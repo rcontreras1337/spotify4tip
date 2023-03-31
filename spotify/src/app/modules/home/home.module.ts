c87f5739ad9b3a0d4429c20f9b968130bf86b128
Ruben Contreras <ruben4tip@hotmail.com>
2023-03-31 02:07:26 -0300

    implementacion shared en homePage
    Se exporta el componente en shared.module que se quiere usar en otro lado, luego en el homePage.module se importa el modulo shred.module, de esta forma se enlaza todo

--- spotify/src/app/modules/home/home.module.ts
+++ spotify/src/app/modules/home/home.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
--- spotify/src/app/modules/home/pages/home-page/home-page.component.ts
+++ spotify/src/app/modules/home/pages/home-page/home-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home-page',
+  selector: 'app-home-page', // nombre para llama a este components en un html
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
