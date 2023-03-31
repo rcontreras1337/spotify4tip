1f47f36b7ecf459fb69c150550d1c92803d43816
Ruben Contreras <ruben4tip@hotmail.com>
2023-03-31 02:40:31 -0300

    se agrega side bar, iconos, nuevas paginas de los modulos

--- spotify/src/app/shared/components/side-bar/side-bar.component.ts
+++ spotify/src/app/shared/components/side-bar/side-bar.component.ts
@@ -7,4 +7,64 @@ import { Component } from '@angular/core';
 })
 export class SideBarComponent {
 
+  //! Aserción de no nulidad, cuidado con esto
+ mainMenu: {
+    defaultOptions: Array<any>;
+    accessLink: Array<any>;
+  } = { defaultOptions: [], accessLink: [] };
+
+  customOptions: Array<any> = [];
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil uil-home',
+        router: ['/'],
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search',
+        router: ['/', 'history'],
+      },
+      {
+        name: 'Tú Biblioteca',
+        icon: 'uil uil-chart',
+        router: ['/', 'favorites'],
+      },
+    ];
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear lista',
+        icon: 'uil-plus-square'
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heart-medical'
+      }
+    ];
+
+    this.customOptions = [
+      {
+        name: 'Mi lista º1',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º2',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º3',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º4',
+        router: ['/']
+      }
+    ];
+  }
+
 }
