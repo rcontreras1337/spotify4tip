e87dcadffa823ab6be216c6a8993a5b419064a13
Rubén Contreras <ruben4tip@hotmail.com>
2023-07-16 00:10:14 -0400

    Router Outlet puesto
    con esta actualizacion creamos dos router outlet el por defecto de la app, ademas generamos otro adentro de el componente HomePageComponenet, en el cual en el htlm agregamos un selector de outlet, al tener modulos, angular busca el mas cercado que en este caso se encuentra en el home page y lo renderiza alli, dejando el principal aparte de esa seccion, de esta manera utilizamos la carga perezosa tambien en esta seccion, haciendo modular las vistas para poder compartir los datos del usuario en las vistas principales sin tener que volver a construir y destruir cada vez esas vistas compartidas

--- spotify/src/app/app-routing.module.ts
+++ spotify/src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 //Valores de rutas
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import(`./modules/auth/auth.module`).then(m => m.AuthModule)
+  },
+  {
  //    path: '',// cuando es vació le digo que vaya al localhost:4200/
  //   component: //indica el componente para que lo importe
+  component: HomePageComponent,
   path: '',
   loadChildren: () => import(`./modules/home/home.module`).then(m => m.HomeModule)
-  }
+  },
+
+
 ];
 
 @NgModule({
--- spotify/src/app/modules/home/home-routing.module.ts
+++ spotify/src/app/modules/home/home-routing.module.ts
@@ -3,13 +3,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
-  {
+ // {
     //path: ':username', // para llegar a esta ruta se concatena, ruta padre(app-routing) y hija, en ese caso
     // le estaríamos pasando un parámetro llamado username, de esta forma cuando escribamos, cualquier cosa
     // en la url nos mandara a aca, homepageComponent, puesto que espera ese parámetro en la url
-    path: '',
-    component: HomePageComponent
-  }
+    //path: '',
+    //component: HomePageComponent
+ // }
+
+ // Cargamos los componentes que se deben cargar en homePageComponente para evitar
+ // que los compartidos se destruyan enteros al momento de pasar a tracks, historial etc
+ {
+  path: 'tracks',
+  loadChildren: () => import('@modules/tracks/tracks.module').then( m => m.TracksModule)
+ },
+ {
+  path: 'history',
+  loadChildren: () => import('@modules/history/history.module').then( m => m.HistoryModule)
+ },
+ {
+  path: 'favorites',
+  loadChildren: () => import('@modules/favorites/favorites.module').then( m => m.FavoritesModule)
+ },
+
 ];
 
 @NgModule({
