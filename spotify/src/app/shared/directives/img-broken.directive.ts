2e7e7769ff6b706751f85cb1616a0b6ca73625bb
Rubén Contreras <ruben4tip@hotmail.com>
2023-07-24 00:02:34 -0400

    directiva para agregar una imagen cuando no se pueda cargar en algun elemento

---
+++ spotify/src/app/shared/directives/img-broken.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: 'img[appImgBroken]'
+})
+export class ImgBrokenDirective {
+  @Input() customImg: string ='';
+
+  @HostListener('error') handleError(): void{
+    const elNative = this.elHost.nativeElement;
+    elNative.src = this.customImg;
+  }
+  constructor(private elHost: ElementRef) { }
+
+}
