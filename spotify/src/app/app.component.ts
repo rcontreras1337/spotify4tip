ac483c4954f8bbcd32737f610b138646ee2c51f3
Rubén Contreras <rcontreras@digitalsoft.cl>
2024-08-16 18:38:28 -0400

    necesario para comenzar

---
+++ spotify/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title: String = 'spotify';
+  name: String = 'Ruben Contreras';
+  // status: string | number = 'fail';
+
+  // car: Car = {
+  //   brand: 'BWM',
+  //   model: 'z4',
+  //   year: 2022
+  // }
+  // car2: Car = {
+  //   brand: 'BWM',
+  //   model: 'z4',
+  // }
+
+  // cars: Array<Car> = [
+  //   {
+  //     brand: 'MERCEDEZ',
+  //     model: 'AMG 200',
+  //   },
+  //   {
+  //     brand: 'BWM',
+  //     model: 'z4',
+  //     year: 2022
+  //   }
+  // ];
+
+}
+
+// interface Car {
+//   brand: string;
+//   model: string;
+//   year?: number;
+// }
