52a95596dead9d7478a6d491ce6a174cf115893c
Rubén Contreras <58892664+rcontreras1337@users.noreply.github.com>
2024-09-06 12:46:25 -0400

    IMPORTA DATARAW JSON PARA PRUEBAS DE ANGULAR

--- spotify/src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ spotify/src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
+import * as dataRaw from '../../../../data/tracks.json'; // Lo que esta en tracks.json se guarda en data
 @Component({
   selector: 'app-tracks-page',
   templateUrl: './tracks-page.component.html',
@@ -12,7 +13,8 @@ export class TracksPageComponent {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-
+    const { data }: any = (dataRaw as any).default;
+    this.mockTracksList = data;
   }
 
 }
