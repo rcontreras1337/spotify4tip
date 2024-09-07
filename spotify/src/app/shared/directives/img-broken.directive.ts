8c274f4b854afe6f3bc12ea8104168b3a139eaea
Rubén Contreras <58892664+rcontreras1337@users.noreply.github.com>
2024-09-07 02:07:14 -0400

    directiva img terminada

--- spotify/src/app/shared/directives/img-broken.directive.ts
+++ spotify/src/app/shared/directives/img-broken.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   // Aplico el selector solo en elementos <img>
@@ -6,6 +6,7 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 })
 export class ImgBrokenDirective {
   private imageReplaced: boolean = false;
+  @Input() customImgIn: string = '';
   // Escuchar al host, en este caso el IMG
   @HostListener('error') handleError(): void {
     const elNative = this.elHost.nativeElement;
@@ -13,7 +14,7 @@ export class ImgBrokenDirective {
     console.log('Imagen rota detectada, cambiando a imagen de reemplazo');
     // Solo reemplazar si aún no se ha reemplazado la imagen
     if (!this.imageReplaced) {
-      elNative.src = 'https://github.com/rcontreras1337/spotify4tip/blob/Inicio_16082024/spotify/src/assets/images/notFound.webp'; // Ruta de imagen de reemplazo
+      elNative.src = this.customImgIn; // Ruta de imagen de reemplazo
       this.imageReplaced = true;  // Marca como reemplazada
       console.log('Imagen reemplazada por notFound.webp');
     }
