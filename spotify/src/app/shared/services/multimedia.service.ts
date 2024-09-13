36e4d04d27f493de3c9930030f4b39145f87c3e0
Rubén Contreras <58892664+rcontreras1337@users.noreply.github.com>
2024-09-13 16:05:48 -0300

    subject y BehaviorSubject

--- spotify/src/app/shared/services/multimedia.service.ts
+++ spotify/src/app/shared/services/multimedia.service.ts
@@ -1,5 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
+import { BehaviorSubject, Observable, Observer, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +7,18 @@ import { Observable, Observer } from 'rxjs';
 export class MultimediaService {
 
   callBack: EventEmitter<any> = new EventEmitter<any>();
+  // myObservable1$: Subject<any> = new Subject();
+  myObservable1$: BehaviorSubject<any> = new BehaviorSubject('');
 
-  myObservable1$: Observable<any> = new Observable(
-    (observer: Observer<any>) => {
+  // myObservable1$: Observable<any> = new Observable(
+  //   (observer: Observer<any>) => {
+  //     observer.next('♒♒♒♒♒')
+  //   }
+  // );
 
-    }
-  );
+  constructor() {
+    this.myObservable1$.next('♒♒♒♒♒');
 
-  constructor() { }
+  }
 
 }
