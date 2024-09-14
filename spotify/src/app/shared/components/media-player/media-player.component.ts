b75aa43f0f2e8b33b8f99e784f8cb62f9a00641c
Rubén Contreras <58892664+rcontreras1337@users.noreply.github.com>
2024-09-14 16:37:13 -0300

    Maquetado spotify listo, faltan funcionalidadees como  ir a la siguiente cancion

--- spotify/src/app/shared/components/media-player/media-player.component.ts
+++ spotify/src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 import { MultimediaService } from '@shared/services/multimedia.service';
 import { Subscription } from 'rxjs';
@@ -9,8 +9,9 @@ import { Subscription } from 'rxjs';
   styleUrl: './media-player.component.css'
 })
 export class MediaPlayerComponent implements OnDestroy{
-
+  @ViewChild('progressBar') progressBar: ElementRef = new ElementRef('');
   listObservers$: Array<Subscription> = [];
+  state: string = 'paused';
 
   constructor(public _multimediaService: MultimediaService) { }
 
@@ -29,6 +30,20 @@ export class MediaPlayerComponent implements OnDestroy{
         console.log("Debo Reproducir 🥡🥡", res);
       }
     );
+    const observer1$ = this._multimediaService.playerStatus$
+      .subscribe(status => this.state = status)
+    this.listObservers$ = [observer1$]
+  }
+
+  handlePosition(event: MouseEvent): void {
+    const elNative: HTMLElement = this.progressBar.nativeElement
+    const { clientX } = event
+    const { x, width } = elNative.getBoundingClientRect()
+    const clickX = clientX - x //TODO: 1050 - x
+    const percentageFromX = (clickX * 100) / width
+    console.log(`Click(x): ${percentageFromX}`);
+    this._multimediaService.seekAudio(percentageFromX)
+
   }
 
 
